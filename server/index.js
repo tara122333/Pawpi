644503f461f9ed30aebae521d7b2db00afebbb32
TARA CHAND KUMAWAT <tarachand122333@gmail.com>
2023-02-10 17:22:36 +0530

    database local connection successfully

--- server/index.js
+++ server/index.js
@@ -2,10 +2,16 @@ import express from 'express';
 
 const app = express();
 
+const port = 4000;
+
+// Database conncetion
+import ConnectDB from'./database/connection';
+
 app.get("/",async(req,res)=>{
     res.json({message : "Success"});
 })
 
-app.listen(4000,()=>{
+app.listen(port,()=>{
     console.log(`server has been started on port 4000`);
+    ConnectDB().then(()=> console.log(`Listening on port ${port}... database has been connected`)).catch((err)=>console.log(`database not connected ${err}`));
 })
