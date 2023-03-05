3cd479a16b5140ccafc2901294ebf1b333a93974
TARA CHAND KUMAWAT <tarachand122333@gmail.com>
2023-03-05 21:21:48 +0530

    database creating processing

--- server/database/AllModels.js
+++ server/database/AllModels.js
@@ -1,10 +1,12 @@
 import {UserModel} from './Users';
 import { UserVerificationModel } from './Users/varification';
 import { ImageModel } from './Images';
+import { ReviewModel } from './Reviews';
 
 
 export {
     UserModel,
     UserVerificationModel,
-    ImageModel
+    ImageModel,
+    ReviewModel
 };
