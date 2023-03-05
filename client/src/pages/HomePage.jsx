8bee5e310d7f58aaf8da8552854870f0afea9073
Shubham Sharma <ishubhamsharma@Shubhams-MacBook-Pro.local>
2023-03-06 00:08:35 +0530

    large screen Shop by Category completed

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,13 +4,57 @@ import Category from '../Components/Category/Category';
 const HomePage = () => {
   return (
     <>
-    <div className="flex items-center flex-col bg-blue-300 w-full h-80">
-        <div className="text-4xl my-4 pb-4">
-            Shop By Category!!
+    <div className="flex items-center flex-col w-full h-full md:px-2">
+      <div className="flex items-start md:items-center flex-col bg-sky-400 w-full h-96 md:h-80 px-4 md:px-10">
+          <div className="text-2xl md:text-4xl my-2 md:my-4 md:pb-4 cursor-pointer">
+              Shop By Category
+          </div>
+          <div className="flex gap-4 ">
+              <Category/>
+          </div>
+      </div>
+
+      <div>
+        <div className="h-full w-full">
+          <img src="https://media.istockphoto.com/id/1399405977/photo/couple-of-friends-a-cat-and-a-dog-run-merrily-through-a-summer-flowering-meadow.jpg?b=1&s=170667a&w=0&k=20&c=1rxFB_UoZlV6kMKbQ11sMpaqeA_d2KFuPn3wB6osbMo="
+          className="h-full w-full"
+          />
         </div>
-        <div className="flex gap-4 mx-6">
-            <Category/>
+        <div>
+          CUSTOMIZE YOUR PET!!
         </div>
+        <button className="text-3xl border">
+          Lets GO!
+        </button>
+      </div>
+
+      <div>
+        FOODS!
+      </div>
+
+      <div>
+        TOYS!
+      </div>
+
+      <div>
+        VET AVAILABLE SOON!!
+      </div>
+
+      <div>
+        CLOTHS
+      </div>
+
+      <div>
+        TOP ACCESSORIES
+      </div>
+
+      <div>
+        KNOW YOUR PET!
+      </div>
+
+      <div>
+        REVIEW
+      </div>
     </div>
     </>
   )
