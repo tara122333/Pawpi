a97ae8058c39374ed84ac623b3376225976a0db8
Shubham Sharma <ishubhamsharma@Shubhams-MacBook-Pro.local>
2023-03-06 14:55:42 +0530

    ShopByCategory Responsive completed

--- client/src/Components/Category/Category.jsx
+++ client/src/Components/Category/Category.jsx
@@ -3,12 +3,31 @@ import React from 'react';
 const CategoryCard=(props)=>{
     return (
         <>
-            <div className="w-full h-52 flex cursor-pointer justify-around bg-yellow-500 rounded-full relative overflow-hidden group">
+            {/*Large Screen*/}
+            <div className="hidden lg:flex w-full h-52 cursor-pointer justify-around bg-yellow-500 rounded-full relative overflow-hidden group">
                 <img classname="w-full h-full" src={props.src} alt="Category Images" />
                 <div className="absolute bg-black/60 h-full w-full flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-500 rounded-full">
                     <div className="text-2xl font-bold text-white">
                         {props.name}
                     </div>
+                </div>
+            </div>
+            {/*Medium screen*/}
+            <div className="hidden md:flex lg:hidden flex-col h-72 w-3/12 items-center justify-around my-4">
+                <div className="h-full w-full flex flex-col bg-yellow-500 rounded-full">
+                    <img className="w-full h-full" src={props.src} alt="CategoryImage"/>
+                </div>
+                <div className="text-2xl">
+                    {props.name}
+                </div>
+            </div>
+            {/*Small Screen*/}
+            <div className="flex flex-col h-48 w-2/5 items-center justify-around md:hidden my-4 ">
+                <div className="h-full w-full flex flex-col bg-yellow-500 rounded-full">
+                    <img className="w-full h-full" src={props.src} alt="CategoryImage"/>
+                </div>
+                <div className="text-xl">
+                    {props.name}
                 </div>
             </div>
         </>
@@ -45,9 +64,21 @@ const Category = () => {
 
   return (
     <>
-        {CategoryImage.map((data)=>(
-            <CategoryCard {...data} />
-        ))}
+        <div className="hidden lg:flex gap-4">
+            {CategoryImage.map((data)=>(
+                <CategoryCard {...data} />
+            ))}
+        </div>
+        <div className="flex flex-wrap md:hidden w-full h-full py-4 justify-around">
+             {CategoryImage.map((data)=>(
+                <CategoryCard {...data} />
+            ))}
+        </div>
+        <div className="md:flex flex-wrap hidden lg:hidden w-full h-full gap-3 justify-around">
+             {CategoryImage.map((data)=>(
+                <CategoryCard {...data} />
+            ))}
+        </div>
     </>
   )
 }
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,23 +4,22 @@ import Category from '../Components/Category/Category';
 const HomePage = () => {
   return (
     <>
-    <div className="flex items-center flex-col w-full h-full md:px-2">
-      <div className="flex items-start md:items-center flex-col bg-sky-400 w-full h-96 md:h-80 px-4 md:px-10">
-          <div className="text-2xl md:text-4xl my-2 md:my-4 md:pb-4 cursor-pointer">
+    <div className=" items-center w-full h-full">
+      <div className="flex px-2 items-center flex-col bg-sky-400 w-full h-full lg:h-80 lg:px-10">
+          <div className="text-2xl lg:text-4xl pt-2 lg:my-2 lg:pb-4 cursor-pointer">
               Shop By Category
           </div>
-          <div className="flex gap-4 ">
-              <Category/>
-          </div>
+          <Category/>
       </div>
 
-      <div>
-        <div className="h-full w-full">
+      <div className="h-full w-full">
+        <div className="h-full w-full mt-14">
           <img src="https://media.istockphoto.com/id/1399405977/photo/couple-of-friends-a-cat-and-a-dog-run-merrily-through-a-summer-flowering-meadow.jpg?b=1&s=170667a&w=0&k=20&c=1rxFB_UoZlV6kMKbQ11sMpaqeA_d2KFuPn3wB6osbMo="
+          alt="PetImage"
           className="h-full w-full"
           />
-        </div>
         <div>
+        </div>
           CUSTOMIZE YOUR PET!!
         </div>
         <button className="text-3xl border">
@@ -60,3 +59,4 @@ const HomePage = () => {
   )
 };
 export default HomePage;
+
