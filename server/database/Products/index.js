850ba309167d5b816e29d7087aa5b458228e8c74
TARA CHAND KUMAWAT <tarachand122333@gmail.com>
2023-03-08 15:46:44 +0530

    Product database has been created

---
+++ server/database/Products/index.js
@@ -0,0 +1,107 @@
+// libraries
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+    brand: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    subtitle: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    subcategory: {
+        type: String,
+        required: true,
+    },
+    animalType: {
+        type: String,
+        required: true,
+    },
+    animalSubType: {
+        type: String,
+        required: true,
+    },
+    size: [{
+        type: String,
+    }],
+
+    isVeg: {
+        type: Boolean,
+        default: false,
+    },
+    site: {
+        type: String,
+    },
+    photos: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Images",
+    }],
+
+    MRP: {
+        type: Number,
+        required: true,
+    },
+    price:
+    {
+        type: Number,
+        default: 150,
+        required: true,
+    },
+
+    coupon: {
+        type: String,
+    },
+
+    discount: {
+        type: Number
+    },
+
+    totalQuantity : {
+        type : Number,
+        required : true,
+    },
+
+    Disclaimer: [{
+        type: String,
+    }],
+
+    instructions: [{
+        type: String,
+    }],
+
+    tip: [{
+        type: String,
+    }],
+
+    features: [{
+        type: String,
+    }],
+
+    reviews: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: "Reviews"
+        }
+    ],
+    seller: {
+        type: mongoose.Types.ObjectId,
+        ref: "Sellers",
+        required: true,
+    },
+
+});
+
+export const ProductModel = mongoose.model("Products", ProductSchema);
