e40517bbbdd037723b2f3d4b3ca893c7d91b24d8
TARA CHAND KUMAWAT <tarachand122333@gmail.com>
2023-03-08 21:51:00 +0530

    database part has been done

--- server/database/AllModels.js
+++ server/database/AllModels.js
@@ -2,11 +2,22 @@ import {UserModel} from './Users';
 import { UserVerificationModel } from './Users/varification';
 import { ImageModel } from './Images';
 import { ReviewModel } from './Reviews';
-
+import { OrderModel } from './Order';
+import { DeliveryModel } from './Delivery';
+import { CartModel } from './Cart';
+import { ProductModel } from './Products';
+import { PetModel } from './Pet';
+import { SellerModel } from './Seller';
 
 export {
     UserModel,
     UserVerificationModel,
     ImageModel,
-    ReviewModel
+    ReviewModel,
+    OrderModel,
+    DeliveryModel,
+    CartModel,
+    ProductModel,
+    PetModel,
+    SellerModel
 };
