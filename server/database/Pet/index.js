e146eb44b344736c9bc9b7916f41e8dbfb1dc33b
TARA CHAND KUMAWAT <tarachand122333@gmail.com>
2023-03-08 21:20:09 +0530

    Pet database has been created

---
+++ server/database/Pet/index.js
@@ -0,0 +1,43 @@
+//  Libraries
+import mongoose from "mongoose";
+
+const PetSchema = new mongoose.Schema({
+    user : {
+        type : mongoose.Types.ObjectId,
+        ref : "Users",
+    },
+    name : {
+        type : String,
+        required : true,
+    },
+    age : {
+        type : Number
+    },
+    category : {
+        type : String,
+        required : true,
+    },
+    subcategory : {
+        type : String,
+        required : true,
+    },
+    hobbies : [{
+        type : String,
+    }],
+
+    favfood : [{
+        type : String,
+    }],
+    
+    photos : [
+        {
+            type : mongoose.Types.ObjectId,
+            ref : "Images",
+        }
+    ],
+
+},
+{timestamps:true}
+);
+
+export const PetModel = mongoose.Schema("Pets",PetSchema);
